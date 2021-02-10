b0534d98c30b801be8c1a423e15a5331df299c30
Nick Downing <nick.downing@lifx.co>
2021-02-11 10:59:47 +1100

    Convert rgb_to_uv_gen*.py, uv_to_rgb_gen_*.py, xy_to_r_gen_py.py to use str.format() with keys instead of f-string formatting, improve tests

--- test/n.sh
+++ test/n.sh
@@ -15,6 +15,14 @@ do
     ../$i/rgb_to_hsbk_srgb$ext .5 .5 .5 5000
     ../$i/rgb_to_hsbk_display_p3$ext .5 .5 .5 5000
     ../$i/rgb_to_hsbk_rec2020$ext .5 .5 .5 5000
+
+    ../$i/rgb_to_uv_srgb$ext .5 .5 .5
+    ../$i/rgb_to_uv_display_p3$ext .5 .5 .5
+    ../$i/rgb_to_uv_rec2020$ext .5 .5 .5
+
+    ../$i/uv_to_rgb_srgb$ext .3 .3
+    ../$i/uv_to_rgb_display_p3$ext .3 .3
+    ../$i/uv_to_rgb_rec2020$ext .3 .3
   ) >out_$i.txt
 
   ext=
